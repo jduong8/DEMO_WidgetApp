af37e5daa2bcf7f6ecd28b0b3e3d370886ba24cb
Duong Jonathan <djonathan91.77@gmail.com>
2023-11-22 13:58:55 +0100

    Initial Project

---
+++ DEMO_WidgetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DEMO_WidgetApp
+//
+//  Created by Jonathan Duong on 22/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DEMO_WidgetApp/DEMO_WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO_WidgetAppApp.swift
+//  DEMO_WidgetApp
+//
+//  Created by Jonathan Duong on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DEMO_WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
